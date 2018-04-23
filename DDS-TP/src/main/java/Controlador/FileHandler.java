d713cdcdeb62b3414666b246591e08b226fa6727
peraltaliliana <peraltalilian@gmail.com>
2018-04-23 18:12:32 -0300

    Agrego clases

--- DDS-TP/src/main/java/Controlador/FileHandler.java
+++ DDS-TP/src/main/java/Controlador/FileHandler.java
@@ -1,5 +1,20 @@
 package Controlador;
 
+import java.io.InputStream;
+
 public class FileHandler {
 
+	
+	  public static InputStream inputFromFile(String path) {
+		  try {
+			  InputStream inputStream = FileHandler.class.getResourceAsStream(path);
+			return inputStream; 
+		  } catch (Exception e) {
+				  e.printStackTrace();
+			  }
+	  return null;
+	  }
+	  
+	  
+	  
 }
--- DDS-TP/src/main/java/Controlador/JsonUtils.java
+++ DDS-TP/src/main/java/Controlador/JsonUtils.java
@@ -1,5 +1,36 @@
 package Controlador;
 
+import java.util.Scanner;
+
+import org.json.JSONObject;
+
+import java.io.InputStream;
+
+
 public class JsonUtils {
 
+	public static String jsonStringFromFile (String path) {
+		Scanner scanner;
+		InputStream in = FileHandler.inputFromFile(path);		
+		scanner = new Scanner(in);
+		String json= scanner.useDelimiter("\\z").next();
+		scanner.close();
+		return json;
+	}
+	
+	public static JSONObject jsonObjectFromFile (String path) {
+		return new JSONObject(jsonStringFromFile(path));
+			
+	}
+	
+	public static boolean objectExists(JSONObject jsonObject, String key) {
+		Object o;
+		try {
+           o = jsonObject.get(key);
+           	} catch(Exception e) {
+           		return false;
+           	}
+		return o != null;
+	}
+	
 }
