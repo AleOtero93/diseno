fa84f0bb2bd5741af53777e10d1e38557866ea60
dangel <darioangelcid@gmail.com>
2018-08-21 22:34:16 -0300

    se modifica clase Simplex

--- DDS-TP/src/main/java/utilidades/Simplex.java
+++ DDS-TP/src/main/java/utilidades/Simplex.java
@@ -3,13 +3,13 @@ package utilidades;
 import java.util.Arrays;
 import java.util.List;
 
-import org.apache.commons.math3.optimization.GoalType;
-import org.apache.commons.math3.optimization.PointValuePair;
-import org.apache.commons.math3.optimization.linear.Relationship;
+import org.apache.commons.math3.optim.PointValuePair;
+import org.apache.commons.math3.optim.linear.Relationship;
+import org.apache.commons.math3.optim.nonlinear.scalar.GoalType;
 
 import entidades.DispositivoInteligente;
 
-public class SimplexFacade {
+public class Simplex {
 	private PointValuePair solucion;
 	
 	public Simplex(List<DispositivoInteligente> dispositivosInteligentes) {
