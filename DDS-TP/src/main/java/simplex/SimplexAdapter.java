ca935f393f1c1c109fb7aa4189f8ec2b3c571316
darioangel <darioangelcid@gmail.com>
2018-08-30 16:01:38 -0300

    se agrega la clase SimplexAdapter

--- DDS-TP/src/main/java/simplex/SimplexAdapter.java
+++ DDS-TP/src/main/java/simplex/SimplexAdapter.java
@@ -1,40 +1,44 @@
 package simplex;
 
-import java.util.Arrays;
-import java.util.List;
-
-import simplex.facade.*;
-import utilidades.Simplex;
-
 import org.apache.commons.math3.exception.TooManyIterationsException;
 import org.apache.commons.math3.optim.PointValuePair;
 import org.apache.commons.math3.optim.linear.Relationship;
 import org.apache.commons.math3.optim.nonlinear.scalar.GoalType;
 
-import entidades.DispositivoInteligente;
+import simplex.facade.SimplexFacade;
+import utilidades.Simplex;
 
-public class SimplexAdapter extends Simplex{
+public class SimplexAdapter extends Simplex {
+
 	private SimplexFacade simplex;
 	private PointValuePair solucion;
 	
-	public SimplexAdapter(GoalType objetivo) {	
+	public SimplexAdapter(GoalType objetivo){
 		SimplexFacade simplexFacade = new SimplexFacade(objetivo, true);
 		this.simplex = simplexFacade;
 	}
-
-	public void funcionEconomica(double ... cantidad){
-		this.simplex.crearFuncionEconomica(cantidad);
-	}
-
-	public void restriccion(Relationship unComparador, double valorAcomprar, double ... consumoDispositivos){
-		this.simplex.agregarRestriccion(unComparador, valorAcomprar, consumoDispositivos);
-	}
-
-	public void resolver() throws TooManyIterationsException{
-		solucion = this.simplex.resolver();
-	}
-
+	
+	@Override
 	public PointValuePair getSolucion() {
 		return this.solucion;
 	}
+
+	@Override
+	public void funcionEconomica(double... cantidad) {
+		this.simplex.crearFuncionEconomica(cantidad);
+		
+	}
+
+	@Override
+	public void restriccion(Relationship unComparador, double valorAcomprar, double... consumoDispositivos) {
+		this.simplex.agregarRestriccion(unComparador, valorAcomprar, consumoDispositivos);
+		
+	}
+
+	@Override
+	public void resolver() throws TooManyIterationsException {
+		solucion = this.simplex.resolver();
+		
+	}
+
 }
